b4338cfb487ba1d892233ed910435d6ffb072f1b
Rob <103075007+Rob-A-B@users.noreply.github.com>
2026-05-07 02:55:45 -0300

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        aux=list(s)
+        stack=[]
+        dici={')':'(' ,']':'[', '}':'{'}
+        print(f' a stack no comeco eh {stack}')
+        for a in aux:
+            print(f' o elemento  atual eh {a}')
+            if a in dici.values():
+                stack.append(a)
+                continue
+            if a in dici and not stack:
+                return False
+            if a  in dici and stack and stack[-1]==dici[a]:
+                stack.pop()
+            else:
+                return False
+            
+        return not stack
